7a2d5e4d1f1907a973434679c169cadf1106d555
Chris Churas <churas.camera@gmail.com>
2019-09-13 13:30:16 -0700

    some changes needed for easier usage by rest service

--- src/test/java/org/ndexbio/communitydetection/rest/model/TestCommunityDetectionRequest.java
+++ src/test/java/org/ndexbio/communitydetection/rest/model/TestCommunityDetectionRequest.java
@@ -14,17 +14,12 @@ public class TestCommunityDetectionRequest {
     public void testGettersAndSetters(){
         CommunityDetectionRequest eq = new CommunityDetectionRequest();
         eq.setAlgorithm("algo");
-        eq.setGraphdirected(Boolean.TRUE);
         eq.setData(new TextNode("hi"));
-        eq.setIpAddress("1.2.3.4");
-        eq.setGraphdirected(Boolean.TRUE);
         HashMap<String, String> cParams = new HashMap<>();
         cParams.put("hi", "bye");
         eq.setCustomParameters(cParams);
         assertEquals("algo", eq.getAlgorithm());
         assertEquals("hi", eq.getData().asText());
-        assertEquals("1.2.3.4", eq.getIpAddress());
-        assertEquals(Boolean.TRUE, eq.getGraphdirected());
         assertEquals("bye", eq.getCustomParameters().get("hi"));
     }
 }
